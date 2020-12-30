2018cac220d8208379dc616b32abe25ddded8d00
wangyuqi <wangyuqi@xiaomi.com>
2020-12-30 13:30:37 +0800

    update

--- example/rank/ml/poly2.py
+++ example/rank/ml/poly2.py
@@ -1,94 +1,81 @@
 import tensorflow as tf
 from absl import app
-import os
+from data.movieLens import train_input_fn, test_input_fn, user_occupation
 
 
 def poly2_model(output_dim):
-    # raw inputs
-    pclass_input = tf.keras.layers.Input(shape=(1,), name="Pclass", dtype=tf.int32)
-    sex_input = tf.keras.layers.Input(shape=(1,), name="Sex", dtype=tf.string)
-    age_input = tf.keras.layers.Input(shape=(1,), name="Age", dtype=tf.float32)
-    sibsp_input = tf.keras.layers.Input(shape=(1,), name="SibSp", dtype=tf.int32)
-    parch_input = tf.keras.layers.Input(shape=(1,), name="Parch", dtype=tf.int32)
-    ticket_input = tf.keras.layers.Input(shape=(1,), name="Ticket", dtype=tf.string)
-    fare_input = tf.keras.layers.Input(shape=(1,), name="Fare", dtype=tf.float32)
-    cabin_input = tf.keras.layers.Input(shape=(1,), name="Cabin", dtype=tf.string)
-    embarked_input = tf.keras.layers.Input(shape=(1,), name="Embarked", dtype=tf.string)
+    timestamp = tf.keras.layers.Input(shape=(1,), name="timestamp", dtype=tf.int64)
+    gender = tf.keras.layers.Input(shape=(1,), name="gender", dtype=tf.string)
+    age = tf.keras.layers.Input(shape=(1,), name="age", dtype=tf.int64)
+    occupation = tf.keras.layers.Input(shape=(1,), name="occupation", dtype=tf.string)
+    zip_code = tf.keras.layers.Input(shape=(1,), name="zip_code", dtype=tf.string)
+    keywords = tf.keras.layers.Input(shape=(None,), name="keywords", dtype=tf.string, ragged=True)
+    publish_year = tf.keras.layers.Input(shape=(1,), name="publishYear", dtype=tf.int64)
+    categories = tf.keras.layers.Input(shape=(None,), name="categories", dtype=tf.string, ragged=True)
 
-    # linear inputs
-    pclass_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=3)(
-        tf.keras.layers.experimental.preprocessing.IntegerLookup(vocabulary=[1, 2, 3], mask_value=None)(pclass_input))
-    sex_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=3)(
-        tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=["male", "female"])(sex_input))
-    age_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=10)(
-        tf.keras.layers.experimental.preprocessing.Discretization(
-            bins=[1, 6, 14, 18, 30, 40, 50, 60, 70])(age_input))
-    sibsp_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=9)(
+    timestamp_hour_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=24)(
+        tf.keras.layers.experimental.preprocessing.Discretization(bins=list(range(24)))(
+            tf.keras.layers.Lambda(function=lambda tensor: tensor // 3600 % 24)(timestamp)))
+    timestamp_week_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=7)(
+        tf.keras.layers.experimental.preprocessing.Discretization(bins=list(range(7)))(
+            tf.keras.layers.Lambda(function=lambda tensor: tensor // 86400 % 7)(timestamp)))
+    gender_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=2, output_mode="binary")(
+        tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=["F", "M"], mask_token=None)(gender))
+    age_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=7)(
         tf.keras.layers.experimental.preprocessing.IntegerLookup(
-            vocabulary=list(range(9)), mask_value=None)(sibsp_input))
-    parch_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=6)(
-        tf.keras.layers.experimental.preprocessing.IntegerLookup(
-            vocabulary=list(range(6)), mask_value=None)(parch_input))
-    ticket_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=200)(
-        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=200)(ticket_input))
-    cabin_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=20)(
-        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=20)(cabin_input))
-    embarked_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=3)(
+            vocabulary=[1, 18, 25, 35, 45, 50, 56], mask_value=None)(age))
+    occupation_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=21)(
         tf.keras.layers.experimental.preprocessing.StringLookup(
-            vocabulary=["S", "C", "Q"], mask_token=None)(embarked_input))
+            vocabulary=list(user_occupation.values()), mask_token=None)(occupation))
+    # 3439
+    zip_code_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=10000)(
+        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=10000)(zip_code))
+    # 4862
+    keywords_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=10000)(
+        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=10000)(keywords))
+    # 1919 ~ 2000
+    publish_year_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=82)(
+        tf.keras.layers.experimental.preprocessing.IntegerLookup(
+            vocabulary=list(range(1919, 2001)), mask_value=None)(publish_year))
+    categories_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=18)(
+        tf.keras.layers.experimental.preprocessing.StringLookup(
+            vocabulary=["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama",
+                        "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War",
+                        "Western"], mask_token=None)(categories))
 
     # cross inputs
-    sex_x_age_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=30)(
-        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=30)(
-            tf.keras.layers.experimental.preprocessing.CategoryCrossing(depth=2)([sex_input, age_input])))
-    sex_x_cabin_input_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=60)(
-        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=60)(
-            tf.keras.layers.experimental.preprocessing.CategoryCrossing(depth=2)([sex_input_layer, cabin_input])))
+    gender_x_age_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=50)(
+        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=50)(inputs=[gender, age]))
+    age_x_publish_year_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=100)(
+        tf.keras.layers.experimental.preprocessing.Hashing(num_bins=100)(inputs=[age, publish_year]))
+    # Hashing with ragged input is not supported yet
+    # age_x_keywords_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=10000)(
+    #     tf.keras.layers.experimental.preprocessing.Hashing(num_bins=10000)(inputs=[age, keywords]))
+    # gender_x_categories_layer = tf.keras.layers.experimental.preprocessing.CategoryEncoding(max_tokens=100)(
+    #     tf.keras.layers.experimental.preprocessing.Hashing(num_bins=100)(inputs=[gender, categories]))
 
-    inputs = tf.keras.layers.Concatenate(axis=1)([
-        pclass_input_layer, sex_input_layer, age_input_layer, sibsp_input_layer, parch_input_layer,
-        ticket_input_layer, cabin_input_layer, embarked_input_layer, fare_input, sex_x_age_input_layer,
-        sex_x_cabin_input_layer
+    inputs = tf.keras.layers.Concatenate(axis=1)(inputs=[
+        timestamp_hour_layer, timestamp_week_layer, gender_layer, age_layer, occupation_layer, zip_code_layer,
+        keywords_layer, publish_year_layer, categories_layer,
+        gender_x_age_layer, age_x_publish_year_layer,
+        # age_x_keywords_layer, gender_x_categories_layer
     ])
     predict = tf.keras.layers.Dense(units=output_dim, activation=tf.keras.activations.sigmoid)(inputs)
     model = tf.keras.models.Model(inputs=[
-        pclass_input, sex_input, age_input, sibsp_input, parch_input, ticket_input, fare_input, cabin_input,
-        embarked_input
+        timestamp, gender, age, occupation, zip_code, keywords, publish_year, categories
     ], outputs=predict)
     return model
 
 
-def input_fn(path, batch_size, num_epochs=1, shuffle_buffer_size=None):
-    return tf.data.experimental.make_csv_dataset(
-        file_pattern=path,
-        batch_size=batch_size,
-        column_names=["PassengerId", "Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare",
-                      "Cabin", "Embarked"],
-        column_defaults=[0, 0, "", "", 0.0, 0, 0, "", 0.0, "", ""],
-        label_name="Survived",
-        select_columns=["Survived", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin",
-                        "Embarked"],
-        field_delim=",",
-        use_quote_delim=True,
-        na_value="null",
-        header=True,
-        num_epochs=num_epochs,
-        shuffle=bool(shuffle_buffer_size),
-        shuffle_buffer_size=shuffle_buffer_size,
-        num_rows_for_inference=0,
-        ignore_errors=False)
-
-
 def main(_):
     model = poly2_model(output_dim=1)
-    train_data_path = os.path.abspath(__file__).replace("example/rank/ml/poly2.py", "data/titanic/train.csv")
-    validate_data_path = os.path.abspath(__file__).replace("example/rank/ml/poly2.py", "data/titanic/validate.csv")
-    train_data = input_fn(train_data_path, batch_size=10)
-    validate_data = input_fn(validate_data_path, batch_size=100)
-    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
-                  loss=tf.keras.losses.binary_crossentropy)
-    model.fit(x=train_data, validation_data=validate_data, epochs=10)
-    tf.keras.utils.plot_model(model, to_file="./poly2.png")
+    train_data = train_input_fn(batch_size=500)
+    validate_data = test_input_fn(batch_size=1000)
+    model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+                  loss=tf.keras.losses.mean_squared_error,
+                  metrics=tf.keras.metrics.RootMeanSquaredError())
+    model.fit(x=train_data, validation_data=validate_data, epochs=2)
+    tf.keras.utils.plot_model(model, to_file="./poly2.png", rankdir="BT")
 
 
 if __name__ == "__main__":
